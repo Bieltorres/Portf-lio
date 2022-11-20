f3aa9c60f7698dcd4ddf4ba73470b62ae8900abe
Gabriel Torres <gt75135@gmail.com>
2022-11-20 18:11:03 -0300

    1º commit

---
+++ assets/js/animation.js
@@ -0,0 +1,16 @@
+let typed = new Typed(".typing", {
+  strings: [
+    "Web developer",
+    "Programmer",
+    "Dev front-end",
+    "Programador web",
+    "Desenvolvedor web",
+    "Programador front-end",
+  ],
+  delay: 3,
+  typedSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+
+export { typed };
---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+import { typed } from "./animation.js";
+
+// const nameUser = prompt("Informe seu nome");
+
+// document.getElementById("name-user").textContent = nameUser;
